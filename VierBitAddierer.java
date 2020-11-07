fd4e78ba59b9fe979977f2eaee1c6e010ce9044c
Thanh <quang.thanh.ta@pr1meti.de>
2020-11-07 16:20:37 +0100

    Implement ha and fa
    - ha stands for half adder
    - fa for full adder

--- VierBitAddierer.java
+++ VierBitAddierer.java
@@ -3,9 +3,20 @@ public class VierBitAddierer{
 		System.loadLibrary("VierBitAddierer");
     }
 
+    public static class AddiererAusgabe {
+        public boolean s,ue;
+
+        @Override
+        public String toString() {
+            return "s: " + (this.s ? 1:0) + ", ue: " + (this.ue ? 1:0);
+        }
+    }
+
     public static native void booleanTestMethod(boolean a);
     public static native boolean nand(boolean a, boolean b);
     public static native boolean xor(boolean a, boolean b);
+    public static native AddiererAusgabe ha(boolean a, boolean b);
+    public static native boolean[] fa(boolean a, boolean b, boolean c);
 
 	public static void main(String[] args){
         System.out.println("Hello");
@@ -23,5 +34,19 @@ public class VierBitAddierer{
         booleanTestMethod(xor(false, true));
         booleanTestMethod(xor(true, false));
         booleanTestMethod(xor(true, true));
+        System.out.println();
+        System.out.println(ha(false, false));
+        System.out.println(ha(false, true));
+        System.out.println(ha(true, false));
+        System.out.println(ha(true, true));
+        System.out.println();
+        System.out.println(fa(false, false, false));
+        System.out.println(fa(false, true, false));
+        System.out.println(fa(true, false, false));
+        System.out.println(fa(true, true, false));
+        System.out.println(fa(false, false, true));
+        System.out.println(fa(false, true, true));
+        System.out.println(fa(true, false, true));
+        System.out.println(fa(true, true, true));
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
+// DO NOT REMOVE ANY COMMENTS
 package main
 
 //#include <jni.h>
+//static jobject allocAddiererAusgabe(JNIEnv * env, jboolean s, jboolean ue) {
+//		jclass clazz = (*env)->FindClass(env, "VierBitAddierer$AddiererAusgabe");
+//		jobject obj = (*env)->AllocObject(env, clazz);
+//		jfieldID fieldS = (*env)->GetFieldID(env, clazz, "s", "Z");
+//		jfieldID fieldUE = (*env)->GetFieldID(env, clazz, "ue", "Z");
+//		(*env)->SetBooleanField(env, obj, fieldS, s);
+//		(*env)->SetBooleanField(env, obj, fieldUE, ue);
+//		return obj;
+//}
+//
 import "C"
 import (
 	"fmt"
@@ -21,6 +32,18 @@ func Java_VierBitAddierer_xor(env *C.JNIEnv, clazz C.jclass, a, b C.jboolean) C.
 	return xor(a, b)
 }
 
+//export Java_VierBitAddierer_ha
+func Java_VierBitAddierer_ha(env *C.JNIEnv, clazz C.jclass, a, b C.jboolean) C.jobject {
+	s, ue := ha(a, b)
+	return C.allocAddiererAusgabe(env, s, ue)
+}
+
+//export Java_VierBitAddierer_fa
+func Java_VierBitAddierer_fa(env *C.JNIEnv, clazz C.jclass, a, b, ue C.jboolean) C.jobject {
+	s2, ue3 := fa(a, b, ue)
+	return C.allocAddiererAusgabe(env, s2, ue3)
+}
+
 func nand(a, b C.jboolean) C.jboolean{
 	if a & b > 0 {
 		return 0
@@ -33,6 +56,17 @@ func xor(a, b C.jboolean) C.jboolean {
 	d, e := nand(a,c), nand(b,c)
 	f := nand(d,e)
 	return f
+}
+
+func ha(a, b C.jboolean) ( C.jboolean, C.jboolean ) {
+	return xor(a,b), a & b
+}
+
+func fa(a, b, ue C.jboolean) (C.jboolean, C.jboolean) {
+	s1, ue1 := ha(a, b)
+	s2, ue2 := ha(s1, ue)
+	ue3 := ue1 | ue2
+	return s2, ue3
 }
 
 func main() {}
